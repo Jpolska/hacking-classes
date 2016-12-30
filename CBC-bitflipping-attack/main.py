8d12531aadd684992999596677f2f7c90e878944
Cheetar <mateusz@sieniawski.net>
2016-12-31 00:15:49 +0100

    Added CBC bitflipping attack code

---
+++ CBC-bitflipping-attack/main.py
@@ -0,0 +1,16 @@
+import os
+
+KEY = os.urandom(16)
+iv = os.urandom(16)
+
+
+def get_cookie(user_data):
+    user_data = user_data.replace(";", "").replace("=", "")
+    return "comment1=cooking%20MCs;userdata=" \\\
+        + user_data + ";comment2=%20like%20a%20pound%20of%20bacon"
+
+
+def validate_user(cookie):
+    if ";admin=true;" in cookie:
+        return True
+    return False
---
+++ CBC-bitflipping-attack/utils.py
@@ -0,0 +1,56 @@
+import os
+
+from Crypto.Cipher import AES
+
+BS = 16  # Block size
+
+
+class InvalidPadding(Exception):
+    pass
+
+
+def enhex(text):
+    return text.encode("hex")
+
+
+def unhex(text):
+    return text.decode("hex")
+
+
+def pad(text):
+    padding_size = BS - len(text) % BS
+    text += "".join(str(chr(padding_size)) for x in range(padding_size))
+    return text
+
+
+def unpad(text):
+    padding_size = ord(text[-1])
+    # Check if padding is correct
+    if filter(lambda x: ord(x) != padding_size, text[-padding_size:]):
+        raise InvalidPadding('Invalid PKCS#7 padding')
+    return text[:-padding_size]
+
+
+def XOR(s1, s2):
+    assert(len(s1) == len(s2))
+    return ''.join(chr(ord(s1[x]) ^ ord(s2[x])) for x in range(len(s1)))
+
+
+def chunk(text):
+    return [text[i:i + BS] for i in range(0, len(text), BS)]
+
+
+def merge_chunks(chunks):
+    return "".join(chunk for chunk in chunks)
+
+
+def Encrypt_AES_CBC(msg, key, iv):
+    msg = pad(msg)
+    cipher = AES.new(key, AES.MODE_CFB, iv)
+    return cipher.encrypt(msg)
+
+
+def Decrypt_AES_CBC(ct, key, iv):
+    cipher = AES.new(key, AES.MODE_CFB, iv)
+    msg = cipher.decrypt(ct)
+    return unpad(msg)
