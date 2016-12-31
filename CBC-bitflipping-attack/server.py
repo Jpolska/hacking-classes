05b15a4119bc1912745c9abdcabe57cbb1502d23
Cheetar <mateusz@sieniawski.net>
2016-12-31 14:48:47 +0100

    Fixed bug that only one user can connect to server at the time

--- CBC-bitflipping-attack/server.py
+++ CBC-bitflipping-attack/server.py
@@ -1,5 +1,6 @@
 import os
 import socket
+import threading
 
 from networking import *
 from utils import *
@@ -13,51 +14,60 @@ def get_cookie(user_data):
         admin access by giving user_data = 'admin=true'
     """
     user_data = user_data.replace(";", "").replace("=", "")
-    return "comment1=cooking%20MCs;userdata=" + user_data + ";comment2=%20like%20a%20pound%20of%20bacon"
+    return "comment1=cooking%20MCs;userdata=" + user_data + \
+        ";comment2=%20like%20a%20pound%20of%20bacon"
 
 
 def validate_admin(cookie):
     return ";admin=true;" in cookie
 
 
-def accept_conn():
-    c, addr = me.accept()
-    print 'Got connection from', addr
-    return c, addr
+class SimpleCookieServer():
 
+    def __init__(self, host, port):
+        self.host = host
+        self.port = port
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.sock.bind((self.host, self.port))
 
-def main():
-    c, addr = accept_conn()
+    def listen(self):
+        self.sock.listen(5)
+        while True:
+            client, address = self.sock.accept()
+            client.settimeout(60)
+            threading.Thread(target=self.listen_to_client,
+                             args=(client, address)).start()
 
-    sendline(c, "Hello user, please insert your data:")
-    user_data = recvline(c)
-    cookie = get_cookie(user_data)
-    enc_cookie = enhex(Encrypt_AES_CBC(cookie, KEY, IV))
-    sendline(c, "Your cookie: " + enc_cookie)
+    def listen_to_client(self, c, addr):
+        sendline(c, "Hello user, please insert your data:")
+        user_data = recvline(c)
+        cookie = get_cookie(user_data)
+        enc_cookie = enhex(Encrypt_AES_CBC(cookie, KEY, IV))
+        sendline(c, "Your cookie: " + enc_cookie)
 
-    sendline(c, "I'm a cookie monster, gimme cookie!")
-    enc_cookie = unhex(recvline(c))
-    try:
-        cookie = Decrypt_AES_CBC(enc_cookie, KEY, IV)
-    except InvalidPadding:
-        sendline(c, "You are tempering with the data...")
+        sendline(c, "I'm a cookie monster, gimme cookie!")
+        try:
+            enc_cookie = unhex(recvline(c))
+        except TypeError:
+            sendline(c, "Non-hexadecimal character found. Quiting...")
+            c.close()
+            exit()
+        try:
+            cookie = Decrypt_AES_CBC(enc_cookie, KEY, IV)
+        except InvalidPadding:
+            sendline(c, "You are tempering with the data. Quitting...")
+            c.close()
+            exit()
+        if validate_admin(cookie):
+            flag = open('flag.txt').read()
+            sendline(c, "Welcome almighty admin!\nHere is your flag: " + flag)
+        else:
+            sendline(c, "You're not admin! Shoo shoo, go away!!!")
+
         c.close()
-    if validate_admin(cookie):
-        flag = open('flag.txt').read()
-        sendline(c, "Welcome almighty admin!\nHere is your flag: " + flag)
-    else:
-        sendline(c, "You're not admin! Shoo shoo, go away!!!")
 
-    c.close()
 
-me = socket.socket()
-host = "localhost"
-port = 9999
-me.bind((host, port))
-
-me.listen(5)
-while True:
-    try:
-        main()
-    except:
-        pass
+if __name__ == "__main__":
+    port_num = input("Port? ")
+    SimpleCookieServer('localhost', port_num).listen()
